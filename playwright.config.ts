bebad24477af1e68a8e3bdf9a726bb5147039405
Laptopmini <laptopmini@gmail.com>
2026-04-16 18:43:00 -0700

    fix: Set latest playwright ralph-node config

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests/e2e",
   testMatch: "**/*.spec.ts",
+  timeout: 10000,
   use: {
     baseURL: "http://localhost:3000",
     headless: true,
@@ -10,6 +11,8 @@ export default defineConfig({
   webServer: {
     command: "npm start",
     url: "http://localhost:3000",
+    stderr: "pipe",
+    stdout: "pipe",
   },
   projects: [
     {
